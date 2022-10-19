0c467621b0d4fe7724a9e956f4146c94c2dab379
TomRomanus <80100929+TomRomanus@users.noreply.github.com>
2022-10-19 21:58:34 +0200

    accelerometer
    added the implementation of the accelerometer

--- main/main.ino
+++ main/main.ino
@@ -1,39 +1,67 @@
+//---- Library Includes -----//
+
+#include <Arduino_LSM9DS1.h>
+
+
 //----- Pinout -----//
+
 const int knockSensorPin = 5;
-const int vibrationPin = 4; // only DAC pin
+const int vibrationPin = 4;   // only DAC pin
+
 
 //----- Constants ----//
+
 const int knockSensorThreshold = 100;
 int vibrationDuration = 1500; // how long the tangible vibrates in ms
 
+
 //----- Variables -----//
+
 bool KnockSensorHit = false;  // true when the knocksensor input gets over knockThreshold, needs to be set to false when read by ble
-int vibrationIntensity = 0;
+int vibrationIntensity = 0;   // value between 0 and 255: 0 = now vibration, 255 = full vibration (needs to be calibrated)
+int acceleroDegreesX = 0;     // value of the angle between -90° and 90°
+int acceleroDegreesY = 0;
+int acceleroDegreesZ = 0;
+
+
+//----- Main Functions -----//
 
 void setup() {
   testOutputInit();
+  //accelerometerInit();
 }
 
 void loop() {
   testOutput();
+  //accelerometer();
   //knockSensor();
   //vibration();
 }
 
+
 //----- Inputs -----//
 
 void accelerometerInit() {
-
+  while (!IMU.begin()) {  // try to start the accelerometer, if it does not connect try again in 100ms
+    delay(100);
+  }
 }
 void accelerometer() { // https://docs.arduino.cc/tutorials/nano-33-ble/imu-accelerometer
+  float x, y, z;
+  if (IMU.accelerationAvailable())  IMU.readAcceleration(x, y, z);
+  x *= 100;
+  y *= 100;
+  z *= 100;
 
+  acceleroDegreesX = map(x, -100, 97, -90, 90);
+  acceleroDegreesY = map(y, -100, 97, -90, 90);
+  acceleroDegreesZ = map(z, -100, 97, -90, 90);
 }
 
 void knockSensor() { // just plain ADC
-  if(analogRead(knockSensorPin) >= knockThreshold) {
-    knockSensorHit = true;
-  }
+  if(analogRead(knockSensorPin) >= knockSensorThreshold) KnockSensorHit = true;
 }
+
 
 //----- Outputs -----//
 
@@ -68,10 +96,9 @@ void sound() { // Adafruit Zero I2S library: https://learn.adafruit.com/adafruit
 }
 
 //----- Bluetooth -----//
-
 void bleInit() {
 
 }
-void ble() { // https://docs.arduino.cc/tutorials/nano-33-ble-sense/ble-device-to-device
+void ble() { // https://assetstore.unity.com/packages/tools/input-management/arduino-bluetooth-plugin-98960#content
 
 }
